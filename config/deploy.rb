991fba1890075b9d41f257a22322bd40bd9c055d
Alex Clarke <aclar48@uwo.ca>
2016-01-31 00:53:31 -0500

    set shared path

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :application,     'alexdclarke.me'
 set :user,            'ubuntu'
 set :puma_threads,    [2, 8]
 set :puma_workers,    0
+shared_path = '/home/ubuntu/rails/apps/alexdclarke.me/shared'
 
 # Don't change these unless you know what you're doing
 set :pty,             true
